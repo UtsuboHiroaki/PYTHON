e49f14a504877b57cdb7975a8d6e14cb97aedb0a
UtsuboHiroaki <hutsubo@gmail.com>
2022-11-26 15:39:15 +0900

    20221126_01

---
+++ python_basic_sec2-main/part10_exceptions/Practice_03.py
@@ -0,0 +1,47 @@
+from datetime import date
+
+
+class DateError(ValueError):
+    def __init__(self, message, dt):
+        self.message = message
+        self.dt = dt
+        super().__init__(message)
+
+    def __str__(self):
+        return f'{self.message}: {self.dt.strftime("%Y/%m/%d")}'
+
+
+class PastDateError(DateError):
+    def __init__(self, dt):
+        super().__init__('過去の日付は指定できません', dt)
+
+
+class HolidayErros(DateError):
+    def __init__(self, dt):
+        super().__init__('休日は指定できません', dt)
+
+
+class FiveTenDayError(DateError):
+    def __init__(self, dt):
+        super().__init__('5,10日は指定できません', dt)
+
+def add_task(task, dt):
+    if dt < date.today():
+        raise PastDateError(dt)
+    if dt.weekday() in [5, 6]:
+        raise HolidayErros(dt)
+    if dt.day % 5 == 0:
+        raise FiveTenDayError(dt)
+    return f'{task} を　{dt.strftime("%Y年%m月%d日")} に必ず実行するとお約束します'
+
+
+if __name__ == '__main__':
+    try:
+        result = add_task('月次決算をまとめる仕事を', date(2011, 11, 26))
+    except DateError as e:
+        print(e)
+    else:
+        print(result)
+    finally:
+        print('処理を終了します')
+
